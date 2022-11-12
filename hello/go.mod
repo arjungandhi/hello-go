4ef9f51acfce912aef38ef72dafc06ba2307aa5a
Arjun Gandhi <contact@arjungandhi.com>
2022-11-11 17:21:34 -0700

    advanced hello

---
+++ hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/arjungandhi/hello-go/hello
+
+go 1.19
---
+++ hello/hello_test.go
@@ -0,0 +1,11 @@
+package hello_test
+
+import (
+	"testing"
+
+	"github.com/arjungandhi/hello-go/hello"
+)
+
+func TestHello(t *testing.T) {
+	hello.Hello()
+}
